a9e5b085dd4154fbce9d6145a8e4b1bc40adc875
benjaiu10 <166828214+benjaiu10@users.noreply.github.com>
2025-04-21 09:08:57 -0300

    TODO

---
+++ Assets/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TMP_InputField inputNombre;
+
+    public TextMeshProUGUI txtSaludo;
+
+    string nombreIngresado;
+    string saludo;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Saludar()
+    {
+        //obtener el texto del inputField
+
+        nombreIngresado = inputNombre.text;
+
+        //concatenar el nombre obtenido con el saludo
+
+        saludo = "Hola " + nombreIngresado;
+
+        //mostrar el saludo en el elemento txt saludo
+
+        txtSaludo.text = saludo;
+
+    }
+}
